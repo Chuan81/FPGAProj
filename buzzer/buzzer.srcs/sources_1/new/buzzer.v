6bdeca15286c8315733fe55dc60251c774ef2869
Chuan81 <819937858@qq.com>
2025-03-02 12:24:26 +0800

    去除.vscode文件夹

---
+++ buzzer/buzzer.srcs/sources_1/new/buzzer.v
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2025/03/02 12:16:25
+// Design Name: 
+// Module Name: buzzer
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module buzzer(
+
+    );
+endmodule
